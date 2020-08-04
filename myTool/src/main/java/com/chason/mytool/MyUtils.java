5b3590b49d32d9ed3ad48551bf2307ca3b4293f5
chason <754702559@qq.com>
2020-08-04 16:49:58 +0800

    add

---
+++ myTool/src/main/java/com/chason/mytool/MyUtils.java
@@ -0,0 +1,14 @@
+package com.chason.mytool;
+
+import android.util.Log;
+
+/**
+ * @Author: chason
+ * @Description:
+ * @CreateDate: 2020/8/4 4:49 PM
+ */
+public class MyUtils {
+    public static void showLog() {
+        Log.i("chason","show log");
+    }
+}
